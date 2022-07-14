59a73a4a43e8b9c3d0498fa519b29b556114ea68
AKovalysko <akovalysko123@gmail.com>
2022-07-14 15:50:11 +0500

    14.07.2022

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import MainPanel from './media/MainPanel.svg'
 class App extends Component
 {
     state = {
-        panel: {id: 1, logo: Logo, isActive: true},
-        water: {id: 2, logo: MainPanel, isActive: false},
-        electricity: {id: 3, logo: MainPanel, isActive: false},
-        air: {id: 4, logo: MainPanel, isActive: false},
-        security: {id: 5, logo: MainPanel, isActive: false},
-        cameras: {id: 6, logo: MainPanel, isActive: false},
-        home: {id: 7, logo: MainPanel, isActive: false}
+        panel: {id: 1, logo: Logo, isActive: true, callBack: () => {}},
+        water: {id: 2, logo: MainPanel, isActive: false, callBack: () => {}},
+        electricity: {id: 3, logo: MainPanel, isActive: false, callBack: () => {}},
+        air: {id: 4, logo: MainPanel, isActive: false, callBack: () => {}},
+        security: {id: 5, logo: MainPanel, isActive: false, callBack: () => {}},
+        cameras: {id: 6, logo: MainPanel, isActive: false, callBack: () => {}},
+        home: {id: 7, logo: MainPanel, isActive: false, callBack: () => {}},
+        exit: {id: 8, logo: MainPanel, isActive: false, callBack: () => {window.close()}}
     }
 
     OnMenuButtonPress(id)
--- src/components/menu.js
+++ src/components/menu.js
@@ -9,7 +9,7 @@ class Menu extends Component
         return (
             <div className="Menu">
                 {Object.keys(this.props.cells).map((cell) => {
-                    return <Cell key={cell} id={this.props.cells[cell].id} click={(id) => {this.props.click(id)}} logo={this.props.cells[cell].logo} maskID={cell} isActive={this.props.cells[cell].isActive}/>
+                    return <Cell key={cell} id={this.props.cells[cell].id} click={(id) => {this.props.click(id)}} logo={this.props.cells[cell].logo} maskID={cell} isActive={this.props.cells[cell].isActive} callBack={this.props.cells[cell].callBack}/>
                 })}
             </div>
         )
@@ -18,7 +18,7 @@ class Menu extends Component
 
 const Cell = (props) => {
     return(
-        <div className={props.isActive ? "Menu-Button Menu-Button_Active" : "Menu-Button"} onClick={() => {props.click(props.id)} }>
+        <div className={props.isActive ? "Menu-Button Menu-Button_Active" : "Menu-Button"} onClick={() => {props.click(props.id); props.callBack()} }>
             <ReactSVG src={props.logo}/>
         </div>
     )
